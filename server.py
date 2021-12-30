0138a344c6b0124acde6f25f420a714af696d376
Paige Milosevic <milosevic@gmail.com>
2021-12-29 20:20:13 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,56 +1,18 @@
-class Pet:
-    def __init__(self, name, type, tricks, noise):
-        self.name = name
-        self.type = type
-        self.tricks = tricks
-        self.health = 100
-        self.energy = 50
-        self.noise = noise
+from classes.ninja import Ninja
+from classes.pet import Pet
 
-    def sleep(self):
-        self.energy += 25
-        return self
-    
-    def eat(self):
-        self.health += 10
-        self.energy += 5
-        return self
-    
-    def play(self):
-        self.health += 5
-        self.energy -= 15
-        return self
-    
-    def makeNoise(self):
-        print(self.noise)
 
-class Ninja:
-    def __init__(self, firstName, lastName, pet, treats, petFood):
-        self.firstname = firstName
-        self.lastname = lastName
-        self.pet = pet
-        self.treats = treats
-        self.petFood = petFood
-    
-    def walk(self):
-        self.pet.play()
-        return self
-    
-    def feed(self):
-        self.pet.eat()
-        return self
-    
-    def bathe(self):
-        self.pet.makeNoise()
-
+stellas_food = ['Nuti-Source, Dinner Dust']
+stellas_treat = ['Greenies, Bully Stick']
 
 stella = Pet('Stella', 'Dog', 'Speak', 'Bark')
 paige = Ninja('Paige', 'Milosevic', stella, 'Dinner Dust', 'NutriSource')
 
-paige.bathe()
 
-# stella.sleep()
-# stella.eat()
-# print(stella.health, stella.energy)
-# stella.play()
-# print(stella.health, stella.energy)
+print(paige.pet.health)
+print(paige.pet.energy)
+paige.bathe()
+paige.feed()
+paige.walk()
+print(paige.pet.health)
+print(paige.pet.energy)
